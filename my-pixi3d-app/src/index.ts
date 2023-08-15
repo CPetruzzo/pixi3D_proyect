6b44f6e02f6ed01204634d0802edfd9fbb34cc79
CPetruzzo <facu03@gmail.com>
2023-08-15 19:09:00 -0300

    chore(impala and road): adds new camera control and new impala and road assets

--- my-pixi3d-app/src/index.ts
+++ my-pixi3d-app/src/index.ts
@@ -1,18 +1,20 @@
 /// <reference path='../global.d.ts' />
 
-import { Application, Assets, Renderer, Resource, Texture } from "pixi.js"
-import { CameraOrbitControl, LightingEnvironment, ImageBasedLighting, Model, Mesh3D, Light, LightType, ShadowCastingLight, ShadowQuality, Sprite3D, SpriteBillboardType, Vec3, Camera } from "pixi3d/pixi7"
+import { Application, Assets, Color, Renderer, Resource, Sprite, Texture } from "pixi.js"
+import { CameraOrbitControl, LightingEnvironment, ImageBasedLighting, Model, Mesh3D, Light, LightType, ShadowCastingLight, ShadowQuality, Sprite3D, SpriteBillboardType, Vec3, Camera, Point3D, Fog } from "pixi3d/pixi7"
+import { Keyboard } from "./Keyboard";
 
 let app = new Application({
   backgroundColor: 0xdddddd, resizeTo: window, antialias: true
 })
 document.body.appendChild(app.view as HTMLCanvasElement)
+Keyboard.initialize(); /* lo llamo una vez y nunca mas */
 
-class Pocho extends Sprite3D {
+export class Pocho extends Sprite3D {
   speedX: number;
   speedY: number;
   speedZ: number;
-  constructor(texture: Texture<Resource> | undefined , areaSize: number) {
+  constructor(texture: Texture<Resource> | undefined, areaSize: number, scale?: Point3D) {
     super(texture);
 
     this.pixelsPerUnit = areaSize;
@@ -28,6 +30,10 @@ class Pocho extends Sprite3D {
 
     // The billboard type is set so the sprite always face the camera.
     this.billboardType = SpriteBillboardType.spherical;
+
+    if (scale) {
+      this.scale = scale;
+    }
   }
 
   distanceFromCamera() {
@@ -76,8 +82,20 @@ const manifest = {
         srcs: "assets/teapot/teapot.gltf",
       },
       {
-        name: "pocho",
+        name: "loli",
         srcs: "assets/cheers1.png",
+      },
+      {
+        name: "pocho",
+        srcs: "assets/cheers2.png",
+      },
+      {
+        name: "impala",
+        srcs: "assets/impala/impala.gltf",
+      },
+      {
+        name: "road",
+        srcs: "assets/road/road.gltf",
       },
     ],
   }]
@@ -86,24 +104,25 @@ const manifest = {
 await Assets.init({ manifest })
 let assets = await Assets.loadBundle("assets")
 
-let model = app.stage.addChild(Model.from(assets.teapot))
-model.y = -0.8
+let model = app.stage.addChild(Model.from(assets.impala))
+model.y = +1
+model.scale.set(30,30,30)
+
+let road = app.stage.addChild(Model.from(assets.road))
+// road.y = +0.5
+// road.scale.set(1,1,10)
+
 
 const pocho: Pocho[] = [];
-for (let i = 0; i < 500; i++) {
-  pocho.push(app.stage.addChild(new Pocho(Texture.from(manifest.bundles[0].assets[3].srcs), 50)));
+// for (let i = 0; i < 500; i++) {
+//   pocho.push(app.stage.addChild(new Pocho(Texture.from(manifest.bundles[0].assets[3].srcs), 150, new Point3D(1,1,1))));
+// }
+for (let i = 0; i < 100; i++) {
+  pocho.push(app.stage.addChild(new Pocho(Texture.from(manifest.bundles[0].assets[4].srcs), 50)));
 }
+
 // So the sprites can be sorted using z-index.
 app.stage.sortableChildren = true;
-
-app.ticker.add(() => {
-  for (let eachpocho of pocho) {
-    eachpocho.update();
-
-    // This will render the bunnies from back to front.
-    eachpocho.zIndex = -eachpocho.distanceFromCamera();
-  }
-});
 
 let ground = app.stage.addChild(Mesh3D.createPlane())
 ground.y = -0.8
@@ -129,5 +148,64 @@ let pipeline = app.renderer.plugins.pipeline
 pipeline.enableShadows(ground, shadowCastingLight)
 pipeline.enableShadows(model, shadowCastingLight)
 
-let control = new CameraOrbitControl(app.view as HTMLCanvasElement);
+// Configura velocidades para el movimiento de la cámara
+const cameraMoveSpeed = 0.1;
+const cameraRotationSpeed = 0.01;
 
+// Crea una instancia del CameraOrbitControl y pásale la cámara y el canvas
+const cameraControl = new CameraOrbitControl(app.view as HTMLCanvasElement);
+let vignette = app.stage.addChild(
+  Sprite.from(
+    "https://raw.githubusercontent.com/jnsmalm/pixi3d-sandbox/master/assets/vignette.png"
+  )
+);
+
+// const color = new Color();
+// const niebla = new Fog(undefined, undefined, undefined);
+// app.stage.addChild(niebla)
+
+app.ticker.add(() => {
+  Object.assign(vignette, {
+    width: app.renderer.width, height: app.renderer.height
+  });
+  for (let eachpocho of pocho) {
+    eachpocho.update();
+
+    // This will render the bunnies from back to front.
+    eachpocho.zIndex = -eachpocho.distanceFromCamera();
+  }
+  // // cameraControl.updateCamera();
+  //   // Calcula el vector de dirección de la cámara
+  //   const cameraDirection = Vec3.set(0, 0, -1);
+  //   cameraDirection.applyEuler(cameraControl.angles.x, cameraControl.angles.y, 0);
+
+  //   // Normaliza el vector para asegurarte de que tiene longitud 1
+  //   cameraDirection.normalize();
+
+  if (Keyboard.state.get("KeyW")) {
+    cameraControl.target.z -= cameraMoveSpeed; // Mueve el objetivo hacia arriba
+  }
+  if (Keyboard.state.get("KeyA")) {
+    cameraControl.target.x -= cameraMoveSpeed; // Mueve el objetivo hacia la izquierda
+  }
+  if (Keyboard.state.get("KeyS")) {
+    cameraControl.target.z += cameraMoveSpeed; // Mueve el objetivo hacia abajo
+  }
+  if (Keyboard.state.get("KeyD")) {
+    cameraControl.target.x += cameraMoveSpeed; // Mueve el objetivo hacia la derecha
+  }
+  if (Keyboard.state.get("Space")) {
+    cameraControl.target.y += cameraMoveSpeed; // Mueve el objetivo hacia la derecha
+  }
+  if (Keyboard.state.get("Enter")) {
+    cameraControl.target.y -= cameraMoveSpeed; // Mueve el objetivo hacia la derecha
+  }
+
+  if (Keyboard.state.get("ArrowUp")){
+    model.z += 1
+  }
+  if (Keyboard.state.get("ArrowDown")){
+    model.z -= 1
+  }
+
+});
